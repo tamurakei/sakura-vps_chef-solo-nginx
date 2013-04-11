6b39a0a45042c15b27d0033827ef4179224b9d47
KentaKatoh <kenta@diverta.co.jp>
2013-04-12 03:51:28 +0900

    ntpdも入れておいた。

--- cookbooks/sshd/recipes/default.rb
+++ cookbooks/sshd/recipes/default.rb
@@ -20,3 +20,12 @@ template "/etc/ssh/sshd_config" do
   notifies :restart, "service[sshd]"
 end
 
+#
+# ntpd
+#
+execute "chkconfig --add mysqld" do
+    command "echo '35 2 * * * /usr/sbin/ntpdate -s ntp.jst.mfeed.ad.jp' >> /var/spool/cron/root"
+end
+
+
+
